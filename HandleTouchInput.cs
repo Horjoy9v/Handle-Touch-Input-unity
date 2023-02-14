0813938baf9fcfbf9d29c4dc6581b6c7d4bcc388
Horjoy9v <125257910+Horjoy9v@users.noreply.github.com>
2023-02-14 13:29:03 +0200

    Update HandleTouchInput.cs

--- HandleTouchInput.cs
+++ HandleTouchInput.cs
@@ -14,7 +14,6 @@ public class SwipeDetector : MonoBehaviour
             touchTimeStart = Time.time;
             startPos = Input.GetTouch(0).position;
         }
-
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
         {
             touchTimeFinish = Time.time;
@@ -22,35 +21,21 @@ public class SwipeDetector : MonoBehaviour
             endPos = Input.GetTouch(0).position;
             direction = endPos - startPos;
 
-            // проверяем длительность жеста
             if (timeInterval > 0.05f)
             {
-                // проверяем направление жеста
                 if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
                 {
                     if (direction.x > 0)
-                    {
                         textComponent.text = "Right swipe";
-                        Debug.Log("Right Swipe");
-                    }
                     else
-                    {
                         textComponent.text = "Left swipe";
-                        Debug.Log("Left Swipe");
-                    }
                 }
                 else
                 {
                     if (direction.y > 0)
-                    {
                         textComponent.text = "Up swipe";
-                        Debug.Log("Up Swipe");
-                    }
                     else
-                    {
                         textComponent.text = "Down swipe";
-                        Debug.Log("Down Swipe");
-                    }
                 }
             }
         }
